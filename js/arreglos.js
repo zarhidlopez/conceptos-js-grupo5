811f8040db583831d5354b29607b99c076fcab32
zarhidlopez <z_andrea25@hotmail.com>
2023-07-14 18:33:35 -0500

    140723

---
+++ js/arreglos.js
@@ -0,0 +1,12 @@
+// los arreglos sirven para contener varias variables
+// sintaxis se declara como una variable let o const 
+
+const auto = [];// array o arreglo vacio 
+let  Auto = ['Deportivo', 'rojo', 'automatico', 250000000];
+
+// mostrar los datos del arreglo
+console.log(auto);
+console.log({Auto});
+
+// ejemplo 2
+const movies = ['Flash', 'Indiana Jones:5', 'Destino Final'];
