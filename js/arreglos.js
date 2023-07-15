9ee61c17e4c07c45a08194a48033dc7b9612986d
zarhidlopez <z_andrea25@hotmail.com>
2023-07-14 19:03:37 -0500

    140723

--- js/arreglos.js
+++ js/arreglos.js
@@ -10,3 +10,28 @@ console.log({Auto});
 
 // ejemplo 2
 const movies = ['Flash', 'Indiana Jones:5', 'Destino Final'];
+//mostrar todas las pelis
+console.log(movies);
+//mostrar solo la peli indiana jones
+console.log(movies[1]);
+
+//combinar arreglos
+let libros = [
+    'Sistemas',
+    'Contabilidad',
+    25000,
+    37000,
+    25000*0.2,
+    ['Wilson', 'Pedro', true, '14/07/23']
+]
+
+//mostrar todo la información de los libros 
+console.log(libros);
+//mostrar la fecha
+console.log(libros[5][3]);
+
+//muestra el ultimo valor de arreglo
+let ultimoValor = libros[libros.length - 1];
+console.log('El último valor es: ', ultimoValor);
+
+
