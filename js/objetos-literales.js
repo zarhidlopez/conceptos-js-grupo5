afc24dd3e39c6b0b4c99eca35e21147c036bdd38
zarhidlopez <z_andrea25@hotmail.com>
2023-07-14 20:16:03 -0500

    140723

---
+++ js/objetos-literales.js
@@ -0,0 +1,16 @@
+// sintaxis de un objeto {} arreglo []
+let libros = {
+    nombre: 'Crimen y castigo',
+    nombre2: 'El principito',
+    precio1: 60000,
+    precio2: 120000,
+    stock: true,
+}
+
+//muestra todas las propiedades del objeto libros
+console.table(libros);
+
+//muestra el libro crimen y castigo
+console.log('El libro es: ', libros.nombre);
+//muestra el libro el principito
+console.log('El libro es: ', libros.nombre2);
